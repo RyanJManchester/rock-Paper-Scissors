49e6138d4807d63a926be59758ffb2bb32c18e31
RyanJManchester <ryanjamesmanchester@gmail.com>
2021-09-16 18:01:38 +1200

    div appended, plays game

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
-let a = playerSelectiion;
 function computerSelection() {
-    let result = "Error";
     let random = Math.floor((Math.random() * 3));
-    if (random == 2) {
-        result = "Scissors";
+    if (random == 0) {
+        return "Rock";
     } else if (random == 1) {
-        result = "Paper";
-    } else { result = "Scissors"};
-    return random;
+        return "Paper";
+    } else {
+        return "Scissors";
+    };
 }
 /* Rock is 1
 Paper is 2
 Scissors is 3 */
-function playRound(a,b) {
+
+let playerTally = 0; let computerTally = 0;
+const tally = document.createElement('div')
+tally.textContent = `player: ${playerTally} | computer: ${computerTally}`;
+const paragraph = document.createElement('div');
+
+let reset = document.querySelector('#reset');
+reset.addEventListener("click", () => {
+    alert("yay!");
+});
+
+function resetfunct() {
+    playerTally = 0;
+    computerTally = 0;
+    body.appendChild(tally);
+    alert("Yay!");
+}
+
+function play(pl) {
     let string;
+    let com = computerSelection();
     if (pl == com) {
-        string = "It's a tie!";
-    } else if (
-        (pl == "Rock" && com == "Paper") ||
-        (pl == "Paper" && com == "Scissors") ||
-        (pl == "Scissors" && com == "Rock")/)
+        return string = "It's a tie!";
+    } else if ( // Only scenarios where player wins
+        (pl == "Rock" && com == "Scissors") ||
+        (pl == "Paper" && com == "Rock") ||
+        (pl == "Scissors" && com == "Paper"))
         {
-        string = `You win! ${pl} beats ${com}`;
+    string = `You win! ${pl} beats ${com}. `;
+        ++playerTally;
     } else {
-        string = `You lose! ${pl} is beaten by ${com}`;
+        string = `You lose! ${pl} is beaten by ${com}. `;
+        ++computerTally;
     }
+    console.log(string);
+    const body = document.querySelector('body');
+    paragraph.textContent = string;
+    tally.textContent = `player: ${playerTally} | computer: ${computerTally}`;
+    body.appendChild(paragraph);
+    body.appendChild(tally);
 }
