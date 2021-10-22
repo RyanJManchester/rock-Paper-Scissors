4957d031ba7dd1cf8caf1df7983a1fc82d1f7d10
RyanJManchester <ryanjamesmanchester@gmail.com>
2021-10-22 21:46:28 +1300

    same as previous commit

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let playerTally = 0; let computerTally = 0;
 const paragraph = document.createElement('div');
 let tally = document.createElement('div');
 
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', function (e) {
+    resetFunct();
+})
+
 
 const body = document.body;
 body.onload = resetFunct();
@@ -12,15 +17,22 @@ reset.onclick = resetFunct();
 body.appendChild(tally);
 
 function resetFunct() {
-    console.log("yay!");
-    alert("YAY!");
+    computerTally = 0, 
+    playerTally = 0,
+    tied = 0;
+    body.removeChild(tally);
+    body.removeChild(paragraph);
+    paragraph.textContent = "Play a game to see your score!";
+    body.insertBefore(paragraph, resetBtn);
 }
 
 function play(pl) {
     let string;
+    let tied = 0;
     let com = computerSelection();
     if (pl == com) {
         string = "It's a tie! Nobody wins.";
+        tied++;
     } else if ( // Only scenarios where player wins
         (pl == "Rock" && com == "Scissors") ||
         (pl == "Paper" && com == "Rock") ||
@@ -34,9 +46,9 @@ function play(pl) {
     }
     console.log(string);
     paragraph.textContent = string;
-    tally.textContent = `player: ${playerTally}
-    | computer: ${computerTally}`;
-    body.appendChild(paragraph);
+    tally.textContent = ` You: ${playerTally}
+    | Tied: ${tied} | Computer: ${computerTally}`;
+    body.insertBefore(paragraph, resetBtn);
     body.appendChild(tally);
 }
 
